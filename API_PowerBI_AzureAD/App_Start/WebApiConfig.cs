ab3d2f55532214283f56cc8ee1b33a7a35e7289d
Filipe Sousa <filipesousamail@gmail.com>
2019-05-23 17:12:12 +0100

    custom auth filter activated

--- API_PowerBI_AzureAD/App_Start/WebApiConfig.cs
+++ API_PowerBI_AzureAD/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace API_PowerBI_AzureAD
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            //config.Filters.Add(new CustomAuthenticationFilter());
+            config.Filters.Add(new CustomAuthenticationFilter());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
